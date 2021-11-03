ebc7f5645dba64b1999c100b61e0b4856cd23518
Erika Silva <erikasilva60@gmail.com>
2021-11-03 14:21:36 -0300

    .

---
+++ 9_2_Exerc2_InserçãoListaOrdenadaSemRepetição.c
@@ -0,0 +1,47 @@
+/*Exercício 2. Inserção em lista ordenada sem repetição
+Crie a função iterativa ins_sr(x,&L), que insere o item x na lista ordenada L somente se x
+não estiver em L. Em seguida, faça um programa para testar o funcionamento da função.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int Item;
+typedef struct no {
+    Item item;
+    struct no *prox;
+} *Lista;
+
+Lista no (Item x, Lista p) {
+    Lista n = malloc(sizeof(struct no));
+    n->item = x;
+    n->prox = p;
+    return n;
+}
+
+void ins_sr (Item x, Lista *L){
+    while( *L != NULL && (*L)->item < x)
+        L = &(*L)->prox;
+    *L = no(x, *L);
+}
+
+void exibe (Lista L){
+    printf("[");
+    while(L != NULL){
+        printf("%d, ", L->item);
+        L = L->prox;
+    }
+    printf("\b\b]");
+    printf("\n");
+}
+
+int main (void){
+    Lista I = NULL;
+    ins_sr (4, &I);
+    ins_sr (1, &I);
+    ins_sr (3, &I);
+    ins_sr (5, &I);
+    ins_sr (2, &I);
+    ins_sr (5, &I);
+    exibe(I);
+    return 0;
+}
